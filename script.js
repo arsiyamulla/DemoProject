9e0cf137d2fe4f4d79e41a6a9fd08ab452a986b7
arsiyamulla <arsiyariyajmulla@gmail.com>
2026-04-07 13:20:36 +0530

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+class ScientificCalculator {
+    constructor(previousOperandElement, currentOperandElement) {
+        this.previousOperandElement = previousOperandElement;
+        this.currentOperandElement = currentOperandElement;
+        this.clear();
+    }
+}
