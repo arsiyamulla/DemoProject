ff7cdf70889c1e178e6f099f669620867d06922c
arsiyamulla <arsiyariyajmulla@gmail.com>
2026-04-08 12:01:56 +0530

    added

--- script.js
+++ script.js
@@ -4,4 +4,25 @@ class ScientificCalculator {
         this.currentOperandElement = currentOperandElement;
         this.clear();
     }
+
+    clear(){
+        this.currentOperand='0';
+        this.previousOperand='';
+        this.operation = undefined;
+    }
+
+    delete(){
+        if(this.currentOperand === '0') return;
+        this.currentOperand = this.currentOperand.toString().slice(0,-1);
+        if (this.currentOperand === '') this.currentOperand = '0';
+    }
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        if (this.currentOperand === '0' && number !== '.') {
+            this.currentOperand = number.toString();
+        } else {
+            this.currentOperand = this.currentOperand.toString() + number.toString();
+        }
+    }
 }
